8c76be36a92e599ad8f37eec37c9417932551dd7
sandrika <sandromaisuradze32@gmail.com>
2020-10-01 00:32:57 +0400

    Added Small Functions

--- src/GetGameInformation.kt
+++ src/GetGameInformation.kt
@@ -1,20 +1,61 @@
+import java.lang.management.GarbageCollectorMXBean
+
 class GetGameInformation {
 
-    fun Game_info(){
+    fun Game_info(Game_Word: String, Player_Name: String, Game_lives: Int, Char_Input:String) {
 
+        /**
+         * Entering game parameters such as Name Word and Lives
+         */
         print("Enter Your Name: ")
 
-        var Player_Name = readLine().toString()
+        val Player_Name = readLine().toString()
 
         print("Hello ${Player_Name}. Let's Hang Some heads.")
 
         print("Enter a Game Word Now: ")
-
-        var Game_Word = readLine().toString().toLowerCase()
+        /**
+         * inputing Word For the game to proceed
+         */
+        val Game_Word = readLine().toString().toLowerCase()
 
         print("Let The Word Be: ${Game_Word}. My Lord! ")
 
-        var Game_lives: Int
+        print("Game Is Starting...")
+        /**
+         * Decidind on non constand life value
+         *
+         * because it will decrease with every mistake
+         */
+        val Game_lives = 8
+        print("Lives Count is: ${Game_lives}.")
+
+        /**
+         * defining variable for individual character input
+         *
+         * to check against "Game_word" variable
+         *
+         */
+        val Char_Input = readLine().toLowerCase()
+        print("Enter character: ${Char_Input}. ")
+
+        if(Game_Word.contains(Char_Input)){
+            print("Congrats! ${Char_Input} is in a Word. ")
+        }
+        else
+        {
+            print("Wrong Letter, Life count is: ${Game_lives - 1} ")
+        }
+
 
     }
+
+
+
+
+
+
+    }
+
+
 }
---
+++ src/RegisterPlayer.kt
@@ -0,0 +1,9 @@
+/**
+ * In ideal case it should save players info in an arrqy
+ */
+
+fun Save_Player_Info(){
+    val game = GetGameInformation()
+    game.Game_info()
+
+}
